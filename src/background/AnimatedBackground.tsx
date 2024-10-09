0781ab191d2aaef2374f08af8814bba955a9172c
Marco Dave <marcodave03@gmail.com>
2024-10-09 22:55:47 +0700

    Update AnimationBackground

--- src/background/AnimatedBackground.tsx
+++ src/background/AnimatedBackground.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import './BubbleBackground.css'; // Import the CSS file for styles
+import './BubbleBackground.css'; 
 
 const BubbleBackground = () => {
     return (
-        <div className="fixed z-0 w-full h-full overflow-hidden bg-gradient-to-t from-gray-200 to-white">
-            {/* Bubbles */}
+        <div className="fixed z-0 w-full h-full overflow-hidden bg-gradient-to-t from-gray-400 to-white">
             <div className="bubbles">
-                {Array.from({ length: 100 }).map((_, index) => ( // Increased from 70 to 100
+                {Array.from({ length: 200 }).map((_, index) => ( 
                     <div key={index} className="bubble"></div>
                 ))}
             </div>
 
-            {/* Circles */}
             <ul className="circles">
-                {Array.from({ length: 30 }).map((_, index) => (
+                {Array.from({ length: 70 }).map((_, index) => (
                     <li key={index} className={`circle-${index + 1}`}></li>
                 ))}
             </ul>
