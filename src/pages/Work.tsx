686b8d9ec44acaed95fe5fb6ab99b06fc504f666
Marco Dave <marcodave03@gmail.com>
2024-10-25 23:10:46 +0700

    Update image

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,9 +2,16 @@ import { useState, useEffect, useRef, RefObject } from "react";
 import { useNavigate } from "react-router-dom";
 import { pageTransitionIn } from "../utils/gsapAnimation";
 import Footer from "../components/Footer";
-import Mockup from "../assets/mockup.jpg";
+import Streamverse from "../assets/Macbook Streamverse.png";
+import CareerSpot from "../assets/Macbook Careerspot.png";
+import ImagineCup from "../assets/Mockup Imaginecup.png";
+import NuatTime from "../assets/Macbook Nuattime.png";
+import NusaTravel from "../assets/Macbook Nusatravel.png";
+import BXplore from "../assets/Mockup BExplore.png";
+import HealthyGood from "../assets/Mockup Healthygoods.png"
 
-type SectionKeys = 'project1' | 'project2' | 'project3';
+
+type SectionKeys = 'project1' | 'project2' | 'project3' | 'project4' | 'project5' | 'project6' | 'project7';
 
 const Work = () => {
   const [activeSection, setActiveSection] = useState<string | null>(null);
@@ -14,8 +21,21 @@ const Work = () => {
     project1: useRef<HTMLDivElement>(null),
     project2: useRef<HTMLDivElement>(null),
     project3: useRef<HTMLDivElement>(null),
+    project4: useRef<HTMLDivElement>(null),
+    project5: useRef<HTMLDivElement>(null),
+    project6: useRef<HTMLDivElement>(null),
+    project7: useRef<HTMLDivElement>(null),
   };
   const [scrollPosition, setScrollPosition] = useState(0);
+  const projectNames = [
+    { id: 'project1', name: 'Streamverse' },
+    { id: 'project2', name: 'NusaTravel' },
+    { id: 'project3', name: 'CareerSpot' },
+    { id: 'project4', name: 'NuatTime' },
+    { id: 'project5', name: 'PreForm' },
+    { id: 'project6', name: 'BXplore' },
+    { id: 'project7', name: 'HealthyGood' },
+  ];
 
   const handleNavigation = (target: string) => {
     const targetText = target.substring(1).toUpperCase() || "HOME";
@@ -38,12 +58,12 @@ const Work = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = Object.keys(sectionRefs) as SectionKeys[]; // Assert keys as SectionKeys
+      const sections = Object.keys(sectionRefs) as SectionKeys[];
       const scrollPosition = window.scrollY;
       let currentSection: string | null = null;
 
       for (const section of sections) {
-        const sectionTop = sectionRefs[section].current?.offsetTop ?? 0; // Use optional chaining
+        const sectionTop = sectionRefs[section].current?.offsetTop ?? 0;
         if (scrollPosition >= sectionTop - 100) {
           currentSection = section;
         }
@@ -68,91 +88,66 @@ const Work = () => {
           <h1 className="text-xl">About</h1>
         </div>
       </div>
-      {/* Landing Page */}
-      {/* <div className="absolute animate-up-down top-6 left-80 transform -translate-y-1/2 -translate-x-1/2 z-20">
-        <img
-          src={Laptop}
-          alt="Marco"
-          className="h-auto w-[40vw] opacity-65"
-        />
-      </div> */}
 
-      <div className="h-screen  text-black hidden lg:block">
-        <div className="overflow-hidden h-[30vw] relative ">
-            <div
-              className="absolute w-[200vw] text-[16vw] font-bold text-center whitespace-nowrap mt-36 hover:opacity-0"
-              style={{
-                transform: `translateX(${-scrollPosition * 0.9}px)`, 
-              }}
-            >
-              - PROJECTS ----------------------------
-            </div>
-          </div>
-
-          <div className="text-6xl text-start w-[80vw] text-wrap mt-8 ml-48 hover:opacity-100 transition-opacity duration-300">
-            Marco Davincent Dermawan
-          </div>
-      </div>
-
-      <div className="flex">
-        <div
-          className="sidebar h-screen w-[25vw] p-4 border-r-[2px] hidden lg:flex" style={{borderColor:"#D6D6D6FF"}}>
-          <div className="space-y-1 m-14">
-            <h2 className="font-semibold text-2xl text-black mb-4">Web Development</h2>
-            <ul className="space-y-1">
-              <li>
-                <a
-                  href="#project1"
-                  className={`block cursor-pointer p-2 ${
-                    activeSection === "project1"
-                      ? "text-xl font-bold text-black"
-                      : "text-lg text-gray-400"
-                  }`}
-                >
-                  Project 1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#project2"
-                  className={`block cursor-pointer p-2 ${
-                    activeSection === "project2"
-                      ? "text-xl font-bold text-black"
-                      : "text-lg text-gray-400"
-                  }`}
-                >
-                  Project 2
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#project3"
-                  className={`block cursor-pointer p-2 ${
-                    activeSection === "project3"
-                      ? "text-xl font-bold text-black"
-                      : "text-lg text-gray-400"
-                  }`}
-                >
-                  Project 3
-                </a>
-              </li>
-            </ul>
+      <div className="h-screen text-black hidden lg:block">
+        <div className="overflow-hidden h-[30vw] relative">
+          <div
+            className="absolute w-[200vw] text-[16vw] font-bold text-center whitespace-nowrap mt-36 hover:opacity-0"
+            style={{
+              transform: `translateX(${-scrollPosition * 0.9}px)`,
+            }}
+          >
+            - PROJECTS ----------------------------
           </div>
         </div>
 
+        <div className="text-6xl text-start w-[80vw] text-wrap mt-8 ml-48 hover:opacity-100 transition-opacity duration-300">
+          Marco Davincent Dermawan
+        </div>
+      </div>
+
+      <div className="flex">
+      <div
+    className="sidebar h-screen w-[15vw] border-r-[2px] hidden lg:flex" style={{ borderColor: "#D6D6D6FF" }}>
+    <div className="space-y-1 m-4">
+      <h2 className="text-2xl text-black mb-4 mt-14">Projects</h2>
+      <ul className="space-y-1">
+        {projectNames.map((project) => (
+          <li key={project.id}>
+            <a
+              href={`#${project.id}`}
+              className={`block cursor-pointer p-2 ${activeSection === project.id ? "text-xl text-black" : "text-lg text-gray-400"}`}
+            >
+              {project.name} {/* Display the actual project name */}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+
         {/* Project Sections */}
         <div className="flex-1 mt-20">
-          <div id="project1" ref={sectionRefs.project1} className="lg:h-screen mt-4 lg:mt-0">
-            <h2 className="text-3xl">Project 1</h2>
-            <img src={Mockup} alt="mockup" className="w-[100vw] lg:w-[90vw] h-auto lg:h-[80vh]" />
+          <div id="project1" ref={sectionRefs.project1} className="mt-4 lg:mt-0 h-screen">
+            <img src={Streamverse} alt="Streamverse mockup" className="w-[90vw] h-[100vh] object-cover" />
           </div>
-          <div id="project2" ref={sectionRefs.project2} className="lg:h-screen mt-4 lg:mt-0">
-            <h2 className="text-3xl">Project 2</h2>
-            <img src={Mockup} alt="mockup" className="w-[100vw] lg:w-[90vw] h-auto lg:h-[80vh]" />
+          <div id="project2" ref={sectionRefs.project2} className="mt-4 lg:mt-0 h-screen">
+            <img src={NusaTravel} alt="NusaTravel mockup" className="w-[90vw] h-[100vh] object-cover" />
           </div>
-          <div id="project3" ref={sectionRefs.project3} className="lg:h-screen mt-4 lg:mt-0">
-            <h2 className="text-3xl">Project 3</h2>
-            <img src={Mockup} alt="mockup" className="w-[100vw] lg:w-[90vw] h-auto lg:h-[80vh]" />
+          <div id="project3" ref={sectionRefs.project3} className="mt-4 lg:mt-0 h-screen">
+            <img src={CareerSpot} alt="CareerSpot mockup" className="w-[90vw] h-[100vh] object-cover" />
+          </div>
+          <div id="project4" ref={sectionRefs.project4} className="mt-4 lg:mt-0 h-screen">
+            <img src={NuatTime} alt="NuatTime mockup" className="w-[90vw] h-[100vh] object-cover" />
+          </div>
+          <div id="project5" ref={sectionRefs.project5} className="mt-4 lg:mt-0 h-screen">
+            <img src={ImagineCup} alt="ImagineCup mockup" className="w-[90vw] h-[100vh] object-cover" />
+          </div>
+          <div id="project6" ref={sectionRefs.project6} className="mt-4 lg:mt-0 h-screen">
+            <img src={BXplore} alt="BXplore mockup" className="w-[90vw] h-[100vh] object-cover" />
+          </div>
+          <div id="project7" ref={sectionRefs.project7} className="mt-4 lg:mt-0 h-screen">
+            <img src={HealthyGood} alt="HealthyGood mockup" className="w-[90vw] h-[100vh] object-cover" />
           </div>
         </div>
       </div>
